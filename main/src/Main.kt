cb2444ee14745262d450067c7b2119115d5f8de7
jbotonakis <jbotonakis@carroll.edu>
2024-12-31 13:43:26 -0700

    Added time delay to view the result.

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -2,26 +2,29 @@
 //Author: John Botonakis
 // Desc: Handles the processing of user inputs, and acts as intermediary between
 //       input and calculation.
-const val running = true
 
-const val tickerTapeSeparator = "\n"
-    val acceptedValues = listOf(
+import kotlin.concurrent.thread //Used to delay the result to be able to see it.
+
+const val running = true
+      val acceptedValues = listOf(
         "+","-","/","*","H","h"
     )
 
-
+//Addition: Adds two numbers together, returning their sum and the equation itself for reference
 fun addition(a: Double, b: Double): Pair<Double, String> {
     val c = a + b
     val t = " $a + $b = $c "
     return Pair(c,t)
 }
 
-
+//Main: Begins, ends, and handles inputs for the program
 fun main() {
     var tickerTape = ""
     //Introductory Information
     println("Calculator")
     println("For help, press (H) to view the FAQ")
+
+    //Main Loop
     while (running) {
     println("Please select your operation:")
         println("""
@@ -33,6 +36,7 @@ fun main() {
     Ticker Tape (T)
     Quit (Q)
 """)
+//Reads in the input and saves it as a variable
     var operatorSelection = readlnOrNull()
         if (acceptedValues.contains(operatorSelection)) {
             operatorSelection = operatorSelection.toString()
@@ -47,9 +51,11 @@ fun main() {
             tickerTape += result.equation
 
             println("Result: ${result.result}")
+            Thread.sleep(2000)
 
 
-        }else if (operatorSelection.isNullOrBlank()  || !acceptedValues.contains(operatorSelection)) {
+
+        } else if (operatorSelection.isNullOrBlank()  || !acceptedValues.contains(operatorSelection)) {
             println("Sorry what you entered was invalid. Please refer to the symbol in parentheses to choose your operation:")
             continue
         }
